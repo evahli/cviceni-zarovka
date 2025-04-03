41dbba7b79f4a7129e4c5ac5a852f0a714f2e8d9
Eva Nunez <evahlivkova@MacBook-Pro-2.local>
2025-04-03 10:33:31 +0200

    upload solution

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('funguju!');
+
+/*Cílem bude, aby se po stisknutí klávesy žárovka na stránce rozsvítila (k elementu žárovky .bulb se přidá třída bulb--on, která pomocí CSS zařídí rozsvícení žárovky). Po té vylepšíte program tak, aby dalším stisknutím klávesy žárovka zase zhasla – a tak stále dokola.*/
+
+const rozsvitit = () => {
+  const zarovka = document.querySelector('.bulb');
+  zarovka.classList.toggle('bulb--on');
+}
+
+document.addEventListener("keydown", rozsvitit)
